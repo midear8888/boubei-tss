51983e691f51467db5d9140fa19bc3f0fb643ed6
holly0 <158677997@qq.com>
2018-04-12 19:23:46 +0800

    fix 修复运单部分页面
    修改运单相关dlg的css，修复接单onchange事件，修复回单管理展示前col多余字段问题

--- src/main/webapp/pages/t/truck.js
+++ src/main/webapp/pages/t/truck.js
@@ -213,7 +213,9 @@ function updateOld(url,tableid,formid,dlgid,title) {
 	// console.log(row)
 	if (row) {
 		$.getJSON(url + row.id, {}, function(select_data) {
+			console.log(select_data)
 			$(formid).form('load', select_data);
+			// $('#client').combobox('setValue',select_data.client)
 			$(dlgid).dialog({
 				"modal": true
 			}).dialog('open').dialog('setTitle', title).dialog('center');
@@ -813,20 +815,20 @@ function numCommafmt(s, n) {
 
 //四舍五入保留2位小数（不够位数，则用0替补）
 function keepTwoDecimalFull(num) {
-  var result = parseFloat(num);
-  if (isNaN(result)) {
-    alert('传递参数错误，请检查！');
-    return false;
-  }
-  result = Math.round(num * 100) / 100;
-  var s_x = result.toString();
-  var pos_decimal = s_x.indexOf('.');
-  if (pos_decimal < 0) {
-    pos_decimal = s_x.length;
-    s_x += '.';
-  }
-  while (s_x.length <= pos_decimal + 2) {
-    s_x += '0';
-  }
-  return s_x;
+	var result = parseFloat(num);
+	if (isNaN(result)) {
+		alert('传递参数错误，请检查！');
+		return false;
+	}
+	result = Math.round(num * 100) / 100;
+	var s_x = result.toString();
+	var pos_decimal = s_x.indexOf('.');
+	if (pos_decimal < 0) {
+		pos_decimal = s_x.length;
+		s_x += '.';
+	}
+	while (s_x.length <= pos_decimal + 2) {
+		s_x += '0';
+	}
+	return s_x;
 }
