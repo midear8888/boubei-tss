14584e0fb4acf076482db45c3e6391fd73c0bc1f
kmy <448535158@qq.com>
2019-01-03 17:12:09 +0800

    fix

--- src/main/java/com/boubei/tssx/wx/WxRegister4GZH.java
+++ src/main/java/com/boubei/tssx/wx/WxRegister4GZH.java
@@ -11,6 +11,7 @@
 package com.boubei.tssx.wx;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -29,51 +30,55 @@ import com.boubei.tss.framework.persistence.ICommonService;
 /**
  * 公众号注册登记
  */
-@WebServlet(urlPatterns="/gzh_reg.in")
+@WebServlet(urlPatterns = "/gzh_reg.in")
 public class WxRegister4GZH extends HttpServlet {
-    private static final long serialVersionUID = -740569423483772472L;
-    
-    Logger log = Logger.getLogger(this.getClass());
-    
-    private ICommonService commService;
-    
+	private static final long serialVersionUID = -740569423483772472L;
+
+	Logger log = Logger.getLogger(this.getClass());
+
+	private ICommonService commService;
+
 	public void init() {
 		commService = (ICommonService) Global.getBean("CommonService");
 	}
-	
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-    		throws ServletException, IOException {
-      doGet(request, response);
-    }
- 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-    		throws ServletException, IOException {
-    	
-    	response.setContentType("text/html;charset=UTF-8");
-    	
-    	log.info(DMUtil.parseRequestParams(request, false));
-    	
-    	String mobile = request.getParameter("mobile");
-    	String code = request.getParameter("code");
-    	String appId = request.getParameter("appId");
-    	String ret = WXUtil.getOpenId4GZH(code, appId);
-    	log.info(ret);
-    	
-    	@SuppressWarnings("unchecked")
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		log.info(DMUtil.parseRequestParams(request, false));
+
+		String mobile = request.getParameter("mobile");
+		String code = request.getParameter("code");
+		String appId = request.getParameter("appId");
+		String ret = WXUtil.getOpenId4GZH(code, appId);
+		log.info(ret);
+
+		@SuppressWarnings("unchecked")
 		Map<String, String> map = (new ObjectMapper()).readValue(ret, Map.class);
-    	
-    	String access_token = map.get("access_token");
-    	String openId = map.get("openid");
-    	
-    	WxGZHBindPhone bindPhone = new WxGZHBindPhone();
-    	bindPhone.setMobile(mobile);
-    	bindPhone.setOpenid(openId);
-    	bindPhone.setAppid(appId);
-    	log.info(bindPhone);
-    	commService.create(bindPhone);
-    	
-    	ret = WXUtil.getUserInfo4GZH(access_token, openId);
-    	log.info(ret);
-    }
+
+		String access_token = map.get("access_token");
+		String openId = map.get("openid");
+
+		@SuppressWarnings("unchecked")
+		List<WxGZHBindPhone> bindPhones = (List<WxGZHBindPhone>) commService.getList("from WxGZHBindPhone where openid=? and appid=?", openId, appId);
+		
+		if (bindPhones.size() == 0) {
+			WxGZHBindPhone bindPhone = new WxGZHBindPhone();
+			bindPhone.setMobile(mobile);
+			bindPhone.setOpenid(openId);
+			bindPhone.setAppid(appId);
+			commService.create(bindPhone);
+		} else {
+			log.info(bindPhones.get(0));
+		}
+
+		ret = WXUtil.getUserInfo4GZH(access_token, openId);
+		log.info(ret);
+	}
 
 }
